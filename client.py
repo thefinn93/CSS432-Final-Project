eac2cdcdf7d78174ee5d5adad6b4d05ea2b2046c
ACM-President <john.paul.quicksall@gmail.com>
2014-05-19 19:20:06 -0700

    Made a basic framework of a client also made both server and client executable

--- client.py
+++ client.py
@@ -1,2 +1,32 @@
 #!/usr/bin/env python
 import socket
+import json
+
+def connect(hostname, port):
+    # For now, the server is hard coded
+    server = socket.getaddrinfo(hostname, port)
+
+    # server[0] will have the socket family (ie INET or INET6)
+    sock = socket.socket(server[0], socket.SOCK_STREAM)
+
+    # server[4] is a tuple in form (ip, port)
+    sock.connect(server[4])
+
+    return (sock, 0)
+
+# Plays 1 game of rock paper scissors
+def playGame(sock, clientid):
+     print "Sorry, I don't know how to play :("
+     return False
+
+if __name__ == "__main__":
+    # Function to connect and authenticate with the server
+    sock, clientid = connect('localhost', 22066)
+
+    # Play until the user doesn't want to anymore
+    donePlaying = False
+    while not donePlaying:
+        donePlaying = playGame(sock, clientid)
+
+    # Close the socket (maybe we should say goodbye?)
+    sock.close()
