57897743014e9237fa7cbb6764cd37bf58e0cef4
Finn <thefinn93@thefinn93.com>
2014-05-27 14:47:28 -0700

    Trying to make it threaded. Should proly just give up and do 343

--- clientGUI.py
+++ clientGUI.py
@@ -4,9 +4,93 @@ import random
 import socket
 import json
 import logging
+import threading
 
 ID_ABOUT = wx.NewId()
 ID_EXIT  = wx.NewId()
+ID_REGISTRATION_SUCCESSFUL = wx.NewId()
+
+# Events
+class RegistrationSuccessEvent(wx.PyEvent):
+    def __init__(self, data):
+        """Init Result Event."""
+        wx.PyEvent.__init__(self)
+        self.SetEventType(EVT_RESULT_ID)
+        self.data = data
+
+class SocketThread(threading.Thread):
+    """A thread to interact with the socket without blocking the GUI"""
+    def __init__(self, notify_window, hostname, port):
+        threading.Thread.__init__(self)
+        self.notify_window = notify_window
+        self.wants_abort = False
+        self.connect(hostname, port)
+        self.start()
+
+    def receive(self):
+        rawmsg = self.sock.recv(4096).strip()
+        try:
+            msg = json.loads(rawmsg)
+            logging.debug("Received: %s" % json.dumps(msg))
+        except ValueError:
+            logging.error("Unreadable JSON received from server: %s" % rawmsg)
+        return msg
+
+    def send(self, action, args = {}):
+        logging.info("Sending %s command: %s" % (action, json.dumps(args)))
+        args['action'] = action
+        self.sock.send(json.dumps(args))
+
+    # Connect to a host and port
+    def connect(self, hostname, port):
+        # Eventually, we should loop through all results and use the first one that
+        # works, but for now
+        server = socket.getaddrinfo(hostname, port)[0]
+        logging.debug("Connecting to %s:%d", server[4][0], server[4][1])
+
+        # server[0] will have the socket family (ie INET or INET6)
+        self.sock = socket.socket(server[0], socket.SOCK_STREAM)
+
+        # server[4] is a tuple in form (ip, port)
+        self.sock.connect(server[4])
+
+    def register(self, name):
+        self.send("register", {"name": name})
+        response = self.receive()
+        registrationSuccessful = False
+        errorMsg = None
+        if "result" in response:
+            if response["result"] == "success":
+                logging.info("Successfully registered!")
+                if "clientid" in response:
+                    self.clientid = response["clientid"]
+                    registrationSuccessful = True
+                else:
+                    errorMsg = "No clientid recieved. Weird, trying again..."
+                    logging.warning("No client ID received. Trying again.")
+            elif response["result"] == "error":
+                if "excuse" in response:
+                    errorMsg = "Failed to register: %s" % response['excuse']
+                    logging.warning("Failed to register. Excuse: %s", response['excuse'])
+                else:
+                    errorMsg = "Failed to register (unknown reason)"
+                    logging.warning("Failed to register, no excuse given")
+            else:
+                errorMsg = "Unrecognized result from server: %s" % response['result']
+                logging.warning("Unrecognized result from server: %s", response['result'])
+        else:
+            logging.error("Did not receive a result from the server: %s" % response)
+        if registrationSuccessful:
+            wx.PostEvent(self.notify_window, RegistrationSuccessEvent)
+            self.listPlayers()
+        else:
+            self.notify_window.(errorMsg)
+
+    def run(self):
+        """Run Worker Thread."""
+        while not self.wants_abort:
+            msg = self.receive()
+
 
 class MainWindow(wx.Frame):
     def __init__(self, parent, id, title):
@@ -40,15 +124,15 @@ class MainWindow(wx.Frame):
         # Show the window
         self.Show()
 
-        self.connect("127.0.0.1", 22066)
+        self.socket = SocketThread(self, "127.0.0.1", 22066)
         self.register()
 
         #self.listPlayers()
 
     def listPlayers(self):
         self.sizer.DeleteWindows()
-        self.sock.sendall(json.dumps({'action':'list'}))
-        response = json.loads(self.sock.recv(1024))
+        self.socket.send("list")
+        response = self.socket.receive()
 
 
         # Build a player list
@@ -74,19 +158,6 @@ class MainWindow(wx.Frame):
         self.sizer.Add(self.playerList, 1, wx.EXPAND)
         self.sizer.Add(buttonSizer, 0, wx.EXPAND)
         self.sizer.Fit(self)
-
-    # Connect to a host and port
-    def connect(self, hostname, port):
-        # Eventually, we should loop through all results and use the first one that
-        # works, but for now
-        server = socket.getaddrinfo(hostname, port)[0]
-        logging.debug("Connecting to %s:%d", server[4][0], server[4][1])
-
-        # server[0] will have the socket family (ie INET or INET6)
-        self.sock = socket.socket(server[0], socket.SOCK_STREAM)
-
-        # server[4] is a tuple in form (ip, port)
-        self.sock.connect(server[4])
 
 
     # Register with the server (prompt the user for a name and send it)
@@ -114,40 +185,9 @@ class MainWindow(wx.Frame):
         logging.debug("Registering with server...")
         name = self.registratonTextBox.GetLineText(0)
         logging.info("Registering as %s", name)
-        self.sock.send(json.dumps({"action": "register", "name": name}))
-        rawresponse = self.sock.recv(1024).strip()
-        logging.debug("Received back %s", rawresponse)
-        registrationSuccessful = False
-        errorMsg = None
-        try:
-            response = json.loads(rawresponse)
-            if "result" in response:
-                if response["result"] == "success":
-                    logging.info("Successfully registered!")
-                    if "clientid" in response:
-                        self.clientid = response["clientid"]
-                        registrationSuccessful = True
-                    else:
-                        errorMsg = "No clientid recieved. Weird, trying again..."
-                        logging.warning("No client ID received. Trying again.")
-                elif response["result"] == "error":
-                    if "excuse" in response:
-                        errorMsg = "Failed to register: %s" % response['excuse']
-                        logging.warning("Failed to register. Excuse: %s", response['excuse'])
-                    else:
-                        errorMsg = "Failed to register (unknown reason)"
-                        logging.warning("Failed to register, no excuse given")
-                else:
-                    errorMsg = "Unrecognized result from server: %s" % response['result']
-                    logging.warning("Unrecognized result from server: %s", response['result'])
-        except ValueError:
-            logging.warning("Failed to parse %s", rawresponse)
-            errorMsg = "Received unreadable message from the server :("
-            print rawresponse
-        if registrationSuccessful:
-            self.listPlayers()
-        else:
-            self.register(errorMsg)
+        self.sizer.DeleteWindows()
+        self.sizer.Add(wx.StaticText(self, label="Registering as %s..." % name))
+        self.socket.register(name)
 
 
     ### Menu Events ###
@@ -157,10 +197,15 @@ class MainWindow(wx.Frame):
               "About Rock, Paper, Scissors", wx.OK)
         d.ShowModal()
         d.Destroy()
+
     # File -> Exit, Exit button
     def OnExit(self, event):
         self.Close(True)
 
-app = wx.App(False)
-frame = MainWindow(None, -1, "Rock, Paper, Scissors")
-app.MainLoop()
+if __name__ == "__main__":
+    # First, configure the logger to dump to client.log
+    logformat = "[%(asctime)s][%(levelname)s] %(message)s"
+    logging.basicConfig(filename="client.log", level=logging.DEBUG, format=logformat)
+    app = wx.App(False)
+    frame = MainWindow(None, -1, "Rock, Paper, Scissors")
+    app.MainLoop()
